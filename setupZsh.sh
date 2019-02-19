dc542f30c0254425be691b151e3151028deec91f
David Denton <daviddenton@users.noreply.github.com>
2019-02-19 18:01:20 +0000

    Delete setupZsh.sh

--- setupZsh.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-touch ~/.zshrc
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo "~/.dotfiles/setupAntigen.sh" > ~/.zshrc
