f50617444d987778e655bc3f0879202fc203f4aa
David Denton <daviddenton@users.noreply.github.com>
2019-02-19 17:21:39 +0000

    Create setupAntigen.sh

---
+++ setupAntigen.sh
@@ -0,0 +1,16 @@
+source /usr/local/share/antigen/antigen.zsh
+
+antigen use oh-my-zsh
+
+antigen bundle command-not-found
+antigen bundle history
+antigen bundle zsh-users/zsh-completions src
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle kennethreitz/autoenv
+antigen bundle git
+antigen bundle pip
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+antigen theme powerlevel9k
+
+antigen apply
