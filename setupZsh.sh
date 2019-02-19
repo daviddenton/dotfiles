9a4d1db4a52ecf933660110290f2f533779c5565
David Denton <denton.david@gmail.com>
2019-02-19 17:36:53 +0000

    permissions

--- setupZsh.sh
+++ setupZsh.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
+
 echo "~/.dotfiles/setupAntigen.sh" > ~/.zshrc
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
