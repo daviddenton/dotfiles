285d6535baf16dacdce96be6684295952d8efc05
David Denton <daviddenton@users.noreply.github.com>
2019-02-19 17:28:11 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,11 @@ set -e
 curl -L https://github.com/daviddenton/dotfiles/archive/master.zip -o master.zip
 unzip master.zip 
 rm -rf master.zip
-pushd dotfiles-master
-
-./setupBrew.sh
-./setupZsh.sh
-
-popd
-
+.dotfiles-master/setupBrew.sh
 rm -rf dotfiles-master
+
+git clone https://github.com/daviddenton/dotfiles.git .dotfiles
+pushd .dotfiles
+git remote rm origin
+git remote add origin git@github.com:daviddenton/dotfiles.git
+popd
