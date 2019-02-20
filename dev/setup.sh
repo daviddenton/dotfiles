874d5f04e244b06d09dd326f44e8af3b454090d3
David <denton.david@gmail.com>
2019-02-20 10:11:31 +0000

    adding config

--- dev/setup.sh
+++ dev/setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir ~/dev
+mkdir -p ~/dev
+
+jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-11.0.2.jdk/Contents/Home
+jenv global 11.0
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -15,4 +15,3 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 antigen theme bhilburn/powerlevel9k powerlevel9k
 
 antigen apply
-
