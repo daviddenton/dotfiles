5e4c8fe2f1fe6249869388ded83332a85efe0bb7
David Denton <denton.david@gmail.com>
2019-02-25 14:57:06 +0000

    meddling

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ set -e
 curl -L https://github.com/daviddenton/dotfiles/archive/master.zip -o master.zip
 unzip master.zip 
 rm -rf master.zip
-.dotfiles-master/setupBrew.sh
+.dotfiles-master/brew/setup.sh
 rm -rf dotfiles-master
 
 git clone https://github.com/daviddenton/dotfiles.git .dotfiles
@@ -13,7 +13,6 @@ git remote rm origin
 git remote add origin git@github.com:daviddenton/dotfiles.git
 popd
 
-.dotfiles/brew/setup.sh
 .dotfiles/git/setup.sh
 .dotfiles/gpg/setup.sh
 .dotfiles/osx/setup.sh
