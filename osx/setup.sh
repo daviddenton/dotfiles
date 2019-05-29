7248808d95d8df9b49cc8f8cea10123f7913a994
David Denton <denton.david@gmail.com>
2019-05-29 14:08:31 +0100

    setup lax mac

--- osx/setup.sh
+++ osx/setup.sh
@@ -2,11 +2,4 @@
 cd $(dirname $0)
 set -e
 
-echo "OSX setup - set name of computer"
-
-read NEW_NAME
-sudo scutil --set ComputerName "${NEW_NAME}"
-sudo scutil --set LocalHostName "${NEW_NAME}"
-sudo scutil --set HostName "${NEW_NAME}"
-
 defaults write com.apple.Dock autohide -bool TRUE; killall Dock
