7e3eed7c8e42a5544390b0f0d4d1fcf298e3e2b8
David Denton <daviddenton@users.noreply.github.com>
2021-01-28 16:56:04 +0000

    Update setup.sh

--- git/setup.sh
+++ git/setup.sh
@@ -12,3 +12,4 @@ read email
 
 git config --global user.email $email
 git config --global user.name $name
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
