43913ad18716ada66a889b6f9a816da71afca163
David Denton <denton.david@gmail.com>
2023-01-27 17:50:53 +0000

    java update for version

--- dev/setup.sh
+++ dev/setup.sh
@@ -5,5 +5,5 @@ set -e
 mkdir -p ~/dev
 
 mkdir -p ~/.jenv/versions
-jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home
-jenv global 11.0
+jenv add /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home
+jenv global 17
