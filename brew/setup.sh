360d26eefad2f6daef8d3a6e2de48b3fd53fe1d6
David Denton <denton.david@gmail.com>
2023-01-27 16:54:19 +0000

    start with .zsh

--- brew/setup.sh
+++ brew/setup.sh
@@ -2,9 +2,16 @@
 cd $(dirname $0)
 set -e
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo Brew setup
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew tap Homebrew/bundle
 brew bundle
+
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
 
 echo "Sign into the App Store then press <enter>"
 read enter
