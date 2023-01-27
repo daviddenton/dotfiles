5fbce543d7460bf4ea29f68af75d4641a93df725
David Denton <denton.david@gmail.com>
2023-01-27 16:41:10 +0000

    start with .zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ rm -rf .dotfiles
 rm -rf dotfiles-master
 
 curl -L https://github.com/daviddenton/dotfiles/archive/master.zip -o master.zip
-unzip master.zip 
+unzip master.zip
 rm -rf master.zip
 ./dotfiles-master/brew/setup.sh
 rm -rf dotfiles-master
@@ -16,9 +16,10 @@ git remote rm origin
 git remote add origin git@github.com:daviddenton/dotfiles.git
 popd
 
-.dotfiles/git/setup.sh
-#.dotfiles/gpg/setup.sh
-.dotfiles/osx/setup.sh
 .dotfiles/zsh/setup.sh
-.dotfiles/dev/setup.sh
+
+#.dotfiles/git/setup.sh
+#.dotfiles/gpg/setup.sh
+#.dotfiles/osx/setup.sh
+#.dotfiles/dev/setup.sh
 
