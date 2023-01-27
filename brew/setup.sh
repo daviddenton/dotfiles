8de0ef63f6894f06d7644f9d1b1d3cbd8b8118d3
David Denton <denton.david@gmail.com>
2023-01-27 16:55:25 +0000

    start with .zsh

--- brew/setup.sh
+++ brew/setup.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 set -e
 
-echo Brew setup
+sudo echo Brew setup
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
